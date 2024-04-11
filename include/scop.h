6a209b314ccca8a384e8a6c68d9a76a77f7c8b7e
Ycaro02 <Ycaro02@hotmail.fr>
2024-04-11 19:35:43 +0200

    Basic token parse

--- include/scop.h
+++ include/scop.h
@@ -4,10 +4,27 @@
 #include "../libft/libft.h"
 
 
-#define TOKEN_COMMENT	'#'			/* Type: string Nb Value: 1 Description: A comment line. */
-#define TOKEN_O 		'o'			/* Type: string Nb Value: 1 Description: The name of the object */
-#define TOKEN_S			's'			/* Type: bool (true or false) Nb Value: 1 Description: The smoothing group state. 'on' to activate, 'off' to deactivate.*/
-#define TOKEN_V			'v'			/* Type: float Nb Value: 3 Description: The coordinates of a 3D vertex.*/
+enum e_obj_token {
+	ENUM_UNKNOWN=0,
+	ENUM_COMMENT=(1 << 0),
+	ENUM_OBJ=(1 << 1),
+	ENUM_SMOOTH=(1 << 2),
+	ENUM_VERTEX=(1 << 3),
+	ENUM_VT=(1 << 4),
+	ENUM_VN=(1 << 5),
+	ENUM_MTLLIB=(1 << 6),
+	ENUM_USEMTL=(1 << 7),
+	ENUM_F=(1 << 8)
+};
+
+/* Token acception in OBJ file */
+
+#define TOKEN_UNKNOWN	"unknown"	/* Type: string Nb Value: 1 Description: Unknown token. */
+
+#define TOKEN_COMMENT	"#"			/* Type: string Nb Value: 1 Description: A comment line. */
+#define TOKEN_OBJ 		"o"			/* Type: string Nb Value: 1 Description: The name of the object */
+#define TOKEN_SMOOTH	"s"			/* Type: bool (true or false) Nb Value: 1 Description: The smoothing group state. on to activate, off to deactivate.*/
+#define TOKEN_VERTEX	"v"			/* Type: float Nb Value: 3 Description: The coordinates of a 3D vertex.*/
 #define TOKEN_VT		"vt"		/* Type: float Nb Value: 2 or 3 Description: The texture coordinates associated with a vertex.*/
 #define TOKEN_VN		"vn"		/* Type: float Nb Value: 3 Description: The coordinates of the normal vector associated with a vertex.*/
 #define TOKEN_MTLLIB	"mtllib"	/* Type: string Nb Value: 1 Description: The name of the material file associated with the object.*/
@@ -20,7 +37,7 @@
  *	Type: uint32_t or int 
  *	Nb Value: variable (3 or more)
 */
-#define TOKEN_F	'f'
+#define TOKEN_F	"f"
 
 typedef struct s_obj_file {
 	char	*o;				/* The name of the object */
--- src/parser/obj_parse.c
+++ src/parser/obj_parse.c
@@ -1,13 +1,37 @@
 #include "../../include/scop.h"
 
-
-static void display_double_char(char **str)
+void display_double_char(char **str)
 {
 	u32 i = 0;
 	while (str[i] != NULL) {
 		ft_printf_fd(1, "%s\n", str[i]);
 		i++;
 	}
+}
+
+
+static uint16_t is_valid_token(char *to_check)
+{
+	char *tokens[] = {
+		TOKEN_COMMENT,
+		TOKEN_OBJ,
+		TOKEN_SMOOTH,
+		TOKEN_VERTEX,
+		TOKEN_VT,
+		TOKEN_VN,
+		TOKEN_MTLLIB,
+		TOKEN_USEMT,
+		TOKEN_F,
+		NULL
+	};
+	for (u32 i = 0; tokens[i]; ++i) {
+		if (ft_strcmp(to_check, tokens[i]) == 0) {
+			ft_printf_fd(1, ORANGE"Token %s is valid, val %d"RESET, to_check, (1 << i));
+			return (1 << i);
+		}
+	}
+	return (0);
+
 }
 
 static int8_t parse_obj_file(char *path)
@@ -17,7 +41,20 @@ static int8_t parse_obj_file(char *path)
 	if ((file = load_file(path)) == NULL) {
 		return (0);
 	}
-	display_double_char(file);
+
+	for (u32 i = 0; file[i]; ++i) {
+		ft_printf_fd(1, YELLOW"\nLine %d: "RESET, i);
+		char **trim = ft_split_trim(file[i], ' ');
+		if (trim != NULL && trim[0] != NULL)  {
+			if (is_valid_token(trim[0]) == 0) {
+				ft_printf_fd(2, RED"Error: Invalid token %s\n"RESET, trim[0]);
+				free_double_char(trim);
+				return (0);
+			}
+		}
+		// display_double_char(trim);
+	}
+	// display_double_char(file);
 	free_double_char(file);
 	return (1);
 }
